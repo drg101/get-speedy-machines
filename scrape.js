c7ed7e97f30c01e9e5596f7b0987103c530353e9
Daniel Reynolds <danielreynolds101@gmail.com>
2022-02-22 15:25:38 -0700

    ufkc you

--- scrape.js
+++ scrape.js
@@ -1,19 +1,38 @@
 import puppeteer from "puppeteer";
 
-const sorted_by_memory = 'https://www.cs.colostate.edu/machinestats/?column=percent_used_memory&order=asc';
-const sorted_by_cpu = 'https://www.cs.colostate.edu/machinestats/?column=cpu&order=asc';
+const machinestats_url = 'https://www.cs.colostate.edu/machinestats';
 
-(async () => {
+const get_top = async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto(sorted_by_memory);
+    await page.goto(machinestats_url);
     const first_n_machines = await page.evaluate((n) => {
         const table_body = document.querySelector('tbody');
-        const rows = [...table_body.children].slice(1, n+1);
-        const machines = rows.map(trow => [...trow.children][0].innerText)
+        const rows = [...table_body.children].slice(1);
+        const machines = rows.map(trow =>  { 
+            const cols = [...trow.children];
+            return {
+                machine: cols[0].innerText,
+                cpu: Number(cols[4].innerText),
+                memory: Number(cols[6].innerText)
+            }
+        })
 
         return machines
-    }, 10);
-    console.log(first_n_machines)
+    });
     await browser.close();
-})();
+    return first_n_machines;
+};
+
+(async () => {
+    while (true) {
+        const top = await get_top();
+        const sorted = top.sort((a,b) => (a.cpu + a.memory) - (b.cpu + b.memory));
+        console.log(sorted)
+        await new Promise(resolve => {
+            setTimeout(() => {
+                resolve()
+            }, 30000)
+        })
+    }
+})()
